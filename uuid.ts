2ab48388efd1a99a74686a327a973ed34222873c
Zambion007 <service@zambion.com>
2016-06-09 10:21:35 +1200

    update parse function

--- uuid.ts
+++ uuid.ts
@@ -2,86 +2,6 @@
 // Project: https://github.com/LiosK/UUID.js
 // Definitions by: Jason Jarrett <https://github.com/staxmanade/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-declare module MUUID {
-
-    interface UUID {
-        intFields:UUIDArray<number>;
-        bitFields:UUIDArray<string>;
-        hexFields:UUIDArray<string>;
-        version:number;
-        bitString:string;
-        hexString:string;
-        urn:string;
-
-
-        /**
-         * Tests if two {@link UUID} objects are equal.
-         * @param {UUID} uuid
-         * @returns {bool} True if two {@link UUID} objects are equal.
-         */
-        equals(uuid:UUID):boolean;
-
-        /**
-         * Returns UUID string representation.
-         * @returns {string} {@link UUID#hexString}.
-         */
-        toString():string;
-    }
-
-    interface UUIDArray<T> extends Array<T> {
-        timeLow:string;
-        timeMid:string;
-        timeHiAndVersion:string;
-        clockSeqHiAndReserved:string;
-        clockSeqLow:string;
-        node:string;
-    }
-
-    /**
-     * The simplest function to get an UUID string.
-     * @returns {string} A version 4 UUID string.
-     */
-    export function generate():string;
-
-    /**
-     * Generates a version 4 {@link UUID}.
-     * @returns {UUID} A version 4 {@link UUID} object.
-     * @since 3.0
-     */
-    export function genV4():UUID;
-
-
-    /**
-     * Generates a version 1 {@link UUID}.
-     * @returns {UUID} A version 1 {@link UUID} object.
-     * @since 3.0
-     */
-    export function genV1():UUID;
-
-    /**
-     * Converts hexadecimal UUID string to an {@link UUID} object.
-     * @param {string} uuid UUID hexadecimal string representation ("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx").
-     * @returns {UUID} {@link UUID} object or null.
-     * @since 3.0
-     */
-    export function parse(uuid:string):UUID;
-
-
-    /**
-     * Re-initializes version 1 UUID state.
-     * @since 3.0
-     */
-    export function resetState():void;
-
-    /**
-     * Reinstalls {@link UUID.generate} method to emulate the interface of UUID.js version 2.x.
-     * @since 3.1
-     * @deprecated Version 2.x. compatible interface is not recommended.
-     */
-    export function makeBackwardCompatible():void;
-}
-
 class UUID {
     intFields:number[];
     bitFields:string[];
@@ -251,7 +171,18 @@ class UUID {
     }
 
     public static parse(strId:string) {
-
+        var r, p = /^\s*(urn:uuid:|\{)?([0-9a-f]{8})-([0-9a-f]{4})-([0-9a-f]{4})-([0-9a-f]{2})([0-9a-f]{2})-([0-9a-f]{12})(\})?\s*$/i;
+        if (r = p.exec(strId)) {
+            var l = r[1] || "", t = r[8] || "";
+            if (((l + t) === "") ||
+                (l === "{" && t === "}") ||
+                (l.toLowerCase() === "urn:uuid:" && t === "")) {
+                return new UUID()._init(parseInt(r[2], 16), parseInt(r[3], 16),
+                    parseInt(r[4], 16), parseInt(r[5], 16),
+                    parseInt(r[6], 16), parseInt(r[7], 16));
+            }
+        }
+        return null;
     }
 
     /**
